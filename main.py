dc419caf76dabd6417e83b38c3c3d5c50bba77fb
SucukMitEi <69sucukmitei69@gmail.com>
2022-11-17 19:18:57 +0100

    example

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from mem import MEM
 
-
-PID = 36961
-
-
+PID = 32911
 
 mem = MEM(PID)
 
+mem.readBytes
+mem.writeBytes
+
+mem.readInt32
+mem.writeInt32
+
+mem.readUInt32
+mem.writeUInt32
+
+mem.readInt64
+mem.writeInt64
+
+mem.readUInt64
+mem.writeUInt64
