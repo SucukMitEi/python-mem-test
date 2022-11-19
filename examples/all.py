dcc3c685e9f4a87d28374ad6207684e248b14a6f
SucukMitEi <SucukMitEi>
2022-11-19 21:00:51 +0100

    Add example all.py

---
+++ examples/all.py
@@ -0,0 +1,20 @@
+from mem import MEM
+
+mem = MEM.fromName("konsole")
+
+for map in mem.maps:
+	print(
+
+		f"""Map: {map}
+
+First 4 Bytes: {mem.readBytes(map[0], 4)}
+
+Int32: {mem.readInt32(map[0])}
+UInt32: {mem.readUInt32(map[0])}
+
+Int64: {mem.readInt64(map[0])}
+UInt64: {mem.readUInt64(map[0])}
+First 40 Characters of String: {mem.readString(map[0], 40)}
+""")
+
+	print()
