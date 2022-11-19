21e552014c7b3f328e52daa1ce26be2771978369
SucukMitEi <SucukMitEi>
2022-11-19 20:57:19 +0100

    example

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ UInt32: {mem.readUInt32(map[0])}
 Int64: {mem.readInt64(map[0])}
 UInt64: {mem.readUInt64(map[0])}
 First 40 Characters of String: {mem.readString(map[0], 40)}
-"""
+""")
 
-	)
 	print()
