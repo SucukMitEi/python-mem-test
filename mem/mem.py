9a81152d7d8fbfcbc931f18eec70f1d777b46935
SucukMitEi <SucukMitEi>
2022-11-19 20:57:12 +0100

    more type hinting

--- mem/mem.py
+++ mem/mem.py
@@ -42,7 +42,7 @@ class Byte(bytes):
 
 
 class MEM:
-	def __init__(self, pid):
+	def __init__(self, pid: int) -> None:
 		self.pid = pid
 
 		self.memfd = open(f"/proc/{pid}/mem", "r+b")
@@ -62,13 +62,13 @@ class MEM:
 			self.maps.append([_from, to, rights])
 
 	@classmethod
-	def fromName(cls, name):
+	def fromName(cls, name: str) -> "MEM":
 		for proc in process_iter():
 			if proc.name() == name:
 				return cls(proc.pid)
 		raise ProcessNotFoundException(f"Couldn't find process {name}")
 
-	def addrValid(self, addr, size, right):
+	def addrValid(self, addr: Int32 | Int64, size: int, right: str) -> bool:
 		for _range in self.maps:
 			# check if address is in the map
 			if addr in range(*_range[:-1]) and addr + size not in range(*_range[:-1]):
@@ -79,7 +79,7 @@ class MEM:
 				return True
 		return False
 
-	def readBytes(self, addr, size) -> List[Byte]:
+	def readBytes(self, addr: Int32 | Int64, size: int) -> List[Byte]:
 		# check if address `addr` is valid for reading `size` bytes
 		if not self.addrValid(addr, size, "r"):
 			raise MemoryReadException("You can not read this address.")
@@ -90,7 +90,7 @@ class MEM:
 		# read `size` bytes
 		return self.memfd.read(size)
 
-	def writeBytes(self, addr, content) -> None:
+	def writeBytes(self, addr: Int32 | Int64, content: bytes) -> None:
 		# check if address `addr` is valid for writing `size` bytes
 		if not self.addrValid(addr, len(content), "w"):
 			raise MemoryWriteException("You can not read this address.")
@@ -101,31 +101,31 @@ class MEM:
 		# write `content` to memory
 		self.memfd.write(content)
 
-	def readInt32(self, addr) -> Int32:
+	def readInt32(self, addr: Int32 | Int64) -> Int32:
 		return unpack("i", self.readBytes(addr, 4))[0]
 
-	def writeInt32(self, addr, value):
+	def writeInt32(self, addr: Int32 | Int64, value: Int32) -> None:
 		self.writeBytes(addr, pack("i", value))
 
-	def readUInt32(self, addr) -> UInt32:
+	def readUInt32(self, addr: Int32 | Int64) -> UInt32:
 		return unpack("I", self.readBytes(addr, 4))[0]
 
-	def writeUInt32(self, addr, value):
+	def writeUInt32(self, addr: Int32 | Int64, value: UInt32) -> None:
 		self.writeBytes(addr, pack("I", value))
 
-	def readInt64(self, addr) -> Int64:
+	def readInt64(self, addr: Int32 | Int64) -> Int64:
 		return unpack("q", self.readBytes(addr, 8))[0]
 
-	def writeInt64(self, addr, value):
+	def writeInt64(self, addr: Int32 | Int64, value: Int64) -> None:
 		self.writeBytes(addr, pack("q", value))
 
-	def readUInt64(self, addr) -> UInt64:
+	def readUInt64(self, addr: Int32 | Int64) -> UInt64:
 		return unpack("Q", self.readBytes(addr, 8))[0]
 
-	def writeUInt64(self, addr, value):
+	def writeUInt64(self, addr: Int32 | Int64, value: UInt64) -> None:
 		self.writeBytes(addr, pack("Q", value))
 
-	def readString(self, addr, size=-1) -> str:
+	def readString(self, addr: Int32 | Int64, size: int = -1) -> str:
 		if size == -1:  # read until NULL character
 			x = b""
 			y = b""
