6652d99d549cf493ffd363ee46a0f801b969bd88
SucukMitEi <SucukMitEi>
2022-11-19 20:21:06 +0100

    add example

--- main.py
+++ main.py
@@ -6,5 +6,18 @@ mem = MEM.fromName("konsole")
 
 for map in mem.maps:
 	print(
-		mem.readString(map[0], map[1])
+		
+		f"""Map: {map}
+		
+First 4 Bytes: {mem.readBytes(map[0], 4)},
+
+Int32: {mem.readInt32(map[0])}
+UInt32: {mem.readUInt32(map[0])}
+
+Int64: {mem.readInt64(map[0])}
+UInt64: {mem.readUInt64(map[0])}
+String: {mem.readString(map[0])}
+"""
+
 	)
+	print()
