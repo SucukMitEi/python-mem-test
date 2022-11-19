61a7fc0e28993df1fe5a1750f3577fe4024696d8
SucukMitEi <SucukMitEi>
2022-11-19 20:48:30 +0100

    update example

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ mem = MEM.fromName("konsole")
 
 for map in mem.maps:
 	print(
-		
+
 		f"""Map: {map}
-		
-First 4 Bytes: {mem.readBytes(map[0], 4)},
+
+First 4 Bytes: {mem.readBytes(map[0], 4)}
 
 Int32: {mem.readInt32(map[0])}
 UInt32: {mem.readUInt32(map[0])}
 
 Int64: {mem.readInt64(map[0])}
 UInt64: {mem.readUInt64(map[0])}
-String: {mem.readString(map[0])}
+First 40 Characters of String: {mem.readString(map[0], 40)}
 """
 
 	)
